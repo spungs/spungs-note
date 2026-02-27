86d90d631c7b327866b9cba5a5a4f8cbd17b43b2
spungs <7564s@naver.com>
2026-02-27 20:25:03 +0900

    style: update page title to Spungs's Note and simplify homepage

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "스펑스노트 🌱",
+    pageTitle: "Spungs의 서재",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
